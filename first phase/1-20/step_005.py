131b6666be3d072311c5d9d11ab28bf22aeb01b7
mhdihmti83 <mahdihemati8383@gmail.com>
2025-07-13 22:29:20 +0330

    step 5

---
+++ first phase/1-20/step_005.py
@@ -0,0 +1,7 @@
+"""برنامه دو عدد را جمع و ضرب و تقسیم میکند"""
+num1 = 10
+num2 = 5
+sum_num = num1+num2
+multiply = num1*num2
+Subtraction = num1 - num2
+print(f"sum: {sum_num} \nmultiply: {multiply} \nSubtraction: {Subtraction}")
