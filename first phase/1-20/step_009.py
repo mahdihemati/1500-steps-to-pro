e2638a641709f314b66e7834577d115bc8637b15
mhdihmti83 <mahdihemati8383@gmail.com>
2025-07-13 22:29:54 +0330

    step 9

---
+++ first phase/1-20/step_009.py
@@ -0,0 +1,14 @@
+"""اهمیت نوشتن رشته ها با تریپل کوتیشن"""
+poem = """There is no door 
+There is no way 
+It is not night 
+not the moon Neither day nor sun
+We stand outside of time 
+With a bitter taste in our pollens
+No one speaks to anyone
+Silence speaks in a thousand languages
+We comment on our dead with a funny design
+And we wait for our turn
+Without any laughter...
+   _ Ahmad Shamlou"""
+print(poem)
