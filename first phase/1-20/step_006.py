55ee707ab6cb566e6d9a44e9c63e51e2be6eb6fc
mhdihmti83 <mahdihemati8383@gmail.com>
2025-07-13 22:29:33 +0330

    step 6

---
+++ first phase/1-20/step_006.py
@@ -0,0 +1,6 @@
+"""این برنامه اهمیت اعداد اعشاری و عملگر تقسیم را نشان میدهد"""
+num1 = 10
+num2 = 3
+division = num1 / num2
+print(
+    f"The result of dividing two numbers is {division} and the type of number is {type(division)}")
